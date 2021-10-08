3b13030c62c54cdf90ccf1ec86d04d423d13ca87
jlouis07 <louissweetlin7@gmail.com>
2021-10-08 12:07:48 +0530

    Check in Functionality

--- src/git/LipGloal.java
+++ src/git/LipGloal.java
@@ -1,10 +1,10 @@
 package git;
 
 public class LipGloal {
-	public static void passWord() {
-		System.out.println("PassWord");
+	public static void checkInDate() {
+		System.out.println("CheckInDate");
 	}
 	public static void main(String[] args) {
-		passWord();
+		checkInDate();
 	}
 }
